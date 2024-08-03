39cd39f8f8404c216048a60bf318bf1a6f6241c9
Awing <awingding.win@gmail.com>
2024-08-03 23:56:22 +0200

    revised code to full ES5 + mw styling practice

--- Common.js
+++ Common.js
@@ -1,112 +1,116 @@
-/* Tout JavaScript présent ici sera exécuté par tous les utilisateurs à chaque chargement de page. */
-var hooks = [];
-var darkMode = [];
-var lightMode = [];
-/*/////////////////////////////////////////////////////*/
-/* Ajustement de la largeur des bandeaux d'information */
-/*/////////////////////////////////////////////////////*/
+( function () {
+	/* Tout JavaScript présent ici sera exécuté par tous les utilisateurs à chaque chargement de page. */
+	var hooks = [];
+	var darkMode = [];
+	var lightMode = [];
+	/* ///////////////////////////////////////////////////// */
+	/* Ajustement de la largeur des bandeaux d'information */
+	/* ///////////////////////////////////////////////////// */
 
-function recalculateSize(content) {
-  var bandeaux = content.find("bandeau-generique");
-  bandeaux.each(function (bandeau) {
-    var previousSibling = bandeau.prev();
-    if (previousSibling) {
-      bandeau.css(
-        width,
-        previousSibling.offsetWidth -
-          parseInt($(bandeau).css("padding-left")) -
-          parseInt($(bandeau).css("padding-right")) +
-          "px"
-      );
-    }
-  });
-}
+	function recalculateSize( content ) {
+		var bandeaux = content.find( 'bandeau-generique' );
+		bandeaux.each( function ( bandeau ) {
+			var previousSibling = bandeau.prev();
+			if ( previousSibling ) {
+				bandeau.css(
+					'width',
+					previousSibling.offsetWidth -
+          parseInt( $( bandeau ).css( 'padding-left' ) ) -
+          parseInt( $( bandeau ).css( 'padding-right' ) ) +
+          'px'
+				);
+			}
+		} );
+	}
 
-hooks.push(recalculateSize);
+	hooks.push( recalculateSize );
 
-/*/////////////////////////////////////////////////////*/
-/*             Gère la couleur des bandeaux            */
-/*/////////////////////////////////////////////////////*/
+	/* ///////////////////////////////////////////////////// */
+	/*             Gère la couleur des bandeaux            */
+	/* ///////////////////////////////////////////////////// */
 
-function darkifyBandeau(content) {
-  var data = content.find(".darkmode-data");
-  var bandeau = content.find(".bandeau-generique");
-  var dark_color = data.data("darkmode-color");
-  if (dark_color != "null") {
-    white_color = bandeau.css("color");
-    bandeau.css("color", dark_color);
-    data.data("whitemode-color", white_color);
-  }
-  var dark_bg_color = data.data("darkmode-bg-color");
-  if (dark_bg_color != "null") {
-    white_bg_color = bandeau.css("background-color");
-    bandeau.css("background-color", dark_bg_color);
-    data.data("whitemode-bg-color", white_bg_color);
-  }
-  var dark_border_color = data.data("darkmode-border-color");
-  if (dark_border_color != "null") {
-    white_border_color = bandeau.css("border-color");
-    bandeau.css("border-color", dark_border_color);
-    data.data("whitemode-border-color", white_border_color);
-  }
-  data.attr("class", ".whitemode-data");
-}
+	function darkifyBandeau( content ) {
+		var data = content.find( '.darkmode-data' );
+		var bandeau = content.find( '.bandeau-generique' );
+		var darkColor = data.data( 'darkmode-color' );
+		if ( darkColor !== 'null' ) {
+			var whiteColor = bandeau.css( 'color' );
+			bandeau.css( 'color', darkColor );
+			data.data( 'whitemode-color', whiteColor );
+		}
+		var darkBgColor = data.data( 'darkmode-bg-color' );
+		if ( darkBgColor !== 'null' ) {
+			var whiteBgColor = bandeau.css( 'background-color' );
+			bandeau.css( 'background-color', darkBgColor );
+			data.data( 'whitemode-bg-color', whiteBgColor );
+		}
+		var darkBorderColor = data.data( 'darkmode-border-color' );
+		if ( darkBorderColor !== 'null' ) {
+			var whiteBorderColor = bandeau.css( 'border-color' );
+			bandeau.css( 'border-color', darkBorderColor );
+			data.data( 'whitemode-border-color', whiteBorderColor );
+		}
+		data.attr( 'class', '.whitemode-data' );
+	}
 
-darkMode.push(darkifyBandeau);
+	darkMode.push( darkifyBandeau );
 
-function clarifyBandeau(content) {
-  var data = content.find(".whitemode-data");
-  var bandeau = content.find(".bandeau-generique");
-  var white_color = data.data("whitemode-color");
-  if (white_color != "null") {
-    dark_color = bandeau.css("color");
-    bandeau.css("color", white_color);
-    data.data("darkmode-color", dark_color);
-  }
-  var white_bg_color = data.data("whitemode-bg-color");
-  if (white_bg_color != "null") {
-    dark_bg_color = bandeau.css("background-color");
-    bandeau.css("background-color", white_bg_color);
-    data.data("darkmode-bg-color", dark_bg_color);
-  }
-  var white_border_color = data.data("whitemode-border-color");
-  if (white_border_color != "null") {
-    dark_border_color = bandeau.css("border-color");
-    bandeau.css("border-color", white_border_color);
-    data.data("darkmode-border-color", dark_border_color);
-  }
-  data.attr("class", ".darkmode-data");
-}
+	function clarifyBandeau( content ) {
+		var data = content.find( '.whitemode-data' );
+		var bandeau = content.find( '.bandeau-generique' );
+		var whiteColor = data.data( 'whitemode-color' );
+		if ( whiteColor !== 'null' ) {
+			var darkColor = bandeau.css( 'color' );
+			bandeau.css( 'color', whiteColor );
+			data.data( 'darkmode-color', darkColor );
+		}
+		var whiteBgColor = data.data( 'whitemode-bg-color' );
+		if ( whiteBgColor !== 'null' ) {
+			var darkBgColor = bandeau.css( 'background-color' );
+			bandeau.css( 'background-color', whiteBgColor );
+			data.data( 'darkmode-bg-color', darkBgColor );
+		}
+		var whiteBorderColor = data.data( 'whitemode-border-color' );
+		if ( whiteBorderColor !== 'null' ) {
+			var darkBorderColor = bandeau.css( 'border-color' );
+			bandeau.css( 'border-color', whiteBorderColor );
+			data.data( 'darkmode-border-color', darkBorderColor );
+		}
+		data.attr( 'class', '.darkmode-data' );
+	}
 
-lightMode.push(clarifyBandeau);
+	lightMode.push( clarifyBandeau );
 
-function actOnBackground(content) {
-  const checkIsDarkSchemePreferred = window.matchMedia && window.matchMedia("(prefers-color-scheme:dark)").matches;
-  if (checkIsDarkSchemePreferred) {
-    for (const hook of darkMode) {
-      hook(content);
-    }
-  } else {
-    for (const hook of lightMode) {
-      hook(content);
-    }
-  }
-}
+	function actOnBackground( content ) {
+		var checkIsDarkSchemePreferred = window.matchMedia && window.matchMedia( '(prefers-color-scheme:dark)' ).matches;
+		if ( checkIsDarkSchemePreferred ) {
+			darkMode.forEach( function ( hook ) {
+				hook( content );
+			} );
+		} else {
+			lightMode.forEach( function ( hook ) {
+				hook( content );
+			} );
+		}
+	}
 
-hooks.push(actOnBackground);
+	hooks.push( actOnBackground );
 
-/*/////////////////////////////////////////////////////*/
-/*                  lance les hooks                    */
-/*/////////////////////////////////////////////////////*/
-mw.hook("wikipage.content").add(function (content) {
-  var pageContent;
-  const PARSER_OUTPUT = content.find(".mw-parser-output");
-  if (PARSER_OUTPUT) {
-    pageContent = PARSER_OUTPUT;
-  } else if (PARSER_OUTPUT.hasClass("mw-parser-output")) {
-    pageContent = PARSER_OUTPUT;
-  } else return;
-  for (const hook of hooks) {
-    hook(pageContent);
-  }
-});
+	/* ///////////////////////////////////////////////////// */
+	/*                  lance les hooks                    */
+	/* ///////////////////////////////////////////////////// */
+	mw.hook( 'wikipage.content' ).add( function ( content ) {
+		var pageContent;
+		var parserOutput = content.find( '.mw-parser-output' );
+		if ( parserOutput ) {
+			pageContent = parserOutput;
+		} else if ( parserOutput.hasClass( 'mw-parser-output' ) ) {
+			pageContent = parserOutput;
+		} else {
+			return;
+		}
+		hooks.forEach( function ( hook ) {
+			hook( pageContent );
+		} );
+	} );
+}() );
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,4 +14,18 @@ const compat = new FlatCompat({
 
 export default [
     ...compat.extends("wikimedia/client", "wikimedia/jquery", "wikimedia/mediawiki"),
+    {
+        rules: {
+            "no-var": "off",
+            "prefer-arrow-callback": "off",
+        },
+        languageOptions: {
+            ecmaVersion: 5,
+            sourceType: "script",
+            globals: {
+                ...globals.browser,
+                ...globals.jquery
+            }
+        }
+    }
 ];
